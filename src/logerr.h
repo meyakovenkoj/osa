e96452738b54806f65d2fb46945a6396b47ddabe
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2021-10-22 15:58:31 +0300

    move to strerror

--- src/logerr.h
+++ src/logerr.h
@@ -3,10 +3,9 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define LOG_ERR(mes) log_err(mes, __FILE__, __LINE__, errno)
-
-extern const char *const sys_errlist[];
 
 void log_err(const char *message, const char *file, int line, int err)
 {
@@ -14,7 +13,7 @@ void log_err(const char *message, const char *file, int line, int err)
     if (message) {
         printf("%s ", message);
     }
-    printf("(errno=%d, %s)\n", err, sys_errlist[err]);
+    printf("(errno=%d, %s)\n", err, strerror(err));
 }
 
 #endif
